87784154a0209e1591b1e0be6260ec658eded7b7
Oleksandra2020 <oleksandrahutor@gmail.com>
2022-06-11 14:40:57 +0300

    Small changes

--- build_precomputed.sh
+++ build_precomputed.sh
@@ -1,2 +1,2 @@
 docker build -f Dockerfile-precomputed . -t run_precomputed:1.0
-docker run --network general-network --rm run_precomputed:1.0
+docker run -v /Users/alexa/Desktop/big_data_project:/big_data_project --network general-network --rm run_precomputed:1.0
--- precomputed.py
+++ precomputed.py
@@ -1,10 +1,10 @@
-from tokenize import group
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, date_trunc
+from pyspark.sql.functions import col, date_trunc, desc
 from cassandra_client import CassandraClient
-from datetime import datetime, timedelta
+from datetime import timedelta
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 spark = SparkSession.builder.appName('big_data_project').getOrCreate()
 
@@ -14,13 +14,13 @@ def read_from_cassandra():
     data = []
     for i in rows:
         data.append(i)
-    return data
+    return spark.createDataFrame(data)
 
 
-def rerport1(df):
+def rerport1():
     grouped_domain = df.withColumn("review_date", date_trunc("hour", col("review_date"))).groupBy(
-        'domain', 'review_date').agg(F.count('page_id')).sort("review_date")
-    # print(grouped_domain.show())
+        'review_date', 'domain').agg(F.count('page_id')).sort("review_date")
+    print(grouped_domain.show())
 
     schema = StructType([
         StructField("time_start", StringType()),
@@ -29,26 +29,97 @@ def rerport1(df):
             StructType([
                     StructField("domain", StringType()),
                     StructField("page_count", LongType())
-                ])
+                    ])
         ))
     ])
 
     time_dct = {}
 
     for d in grouped_domain.collect():
-        domain, review_date, count = d[0], d[1], d[2]
+        domain, review_date, count = d[1], d[0], d[2]
         if review_date not in time_dct:
             time_dct[review_date] = []
         time_dct[review_date].append([domain, count])
 
-        for k in time_dct:
-            data = [str(k), str(k+timedelta(hours=1)), time_dct[k]]
-
-        res_df = spark.createDataFrame(data=data, schema=schema)
+    for k in time_dct:
+        data = [str(k), str(k+timedelta(hours=1)), time_dct[k]]
+        print(data)
+        res_df = spark.createDataFrame(data=[data], schema=schema)
         res_df.printSchema()
-        # res_df.show(truncate=False)
+        res_df.show(truncate=False)
 
-        res_df.write.mode("Overwrite").json(f"./{str(review_date)}")
+        # r = str(k)[11:13] # retrieve hour
+        # res_df.write.mode("Overwrite").json(f"./a_{r}")
+
+
+def rerport2():
+    grouped_domain = df.withColumn("review_date", date_trunc("hour", col("review_date"))).groupBy(
+        'review_date', 'domain').agg(F.count('user_is_bot')).sort("review_date")
+    print(grouped_domain.show())
+
+    schema = StructType([
+        StructField("time_start", StringType()),
+        StructField("time_end", StringType()),
+        StructField("statistics", ArrayType(
+            StructType([
+                    StructField("domain", StringType()),
+                    StructField("created_by_bots", LongType())
+                    ])
+        ))
+    ])
+
+    bot_dct = {}
+
+    for d in grouped_domain.collect():
+        domain, review_date, bot = d[1], d[0], d[2]
+        if review_date not in bot_dct:
+            bot_dct[review_date] = []
+        bot_dct[review_date].append([domain, bot])
+
+    for k in bot_dct:
+        data = [str(k), str(k+timedelta(hours=1)), bot_dct[k]]
+        print(data)
+        res_df = spark.createDataFrame(data=[data], schema=schema)
+        res_df.printSchema()
+        res_df.show(truncate=False)
+
+        # r = str(k)[11:13] # retrieve hour
+        # res_df.write.mode("Overwrite").json(f"./b_{r}")
+
+
+def rerport3():
+    grouped_domain = df.withColumn("review_date", date_trunc("hour", col("review_date"))).groupBy(
+        'user_id').agg(F.count("page_id")).sort(desc("count(page_id)")).limit(20)
+
+    users = [u[0] for u in grouped_domain.collect()]
+    filtered_users = df.filter(df.user_id.isin(users)).select("user_text", "user_id", "review_date", "page_title")
+
+    schema = StructType([
+        StructField("user_name", StringType()),
+        StructField("user_id", LongType()),
+        StructField("time_start", StringType()),
+        StructField("time_end", StringType()),
+        StructField("page_count", LongType()),
+        StructField("page_titles", ArrayType(
+            StructType([
+                    StructField("page_title", StringType())
+                ])
+        ))
+    ])
+    data = []
+    for user in users:
+        f = filtered_users.select("user_text", "review_date", "page_title").where(filtered_users.user_id == user)
+        page_titles = []
+        for i in f.collect():
+            page_titles.append(i['page_title'])
+
+        data.append([i["user_text"], user, str(i['review_date']), str(i['review_date']+timedelta(hours=6)), len(page_titles), page_titles])
+
+    res_df = spark.createDataFrame(data=data, schema=schema)
+    res_df.printSchema()
+    res_df.show(truncate=False)
+
+    # res_df.write.mode("Overwrite").json(f"./report3_hour_{}")
 
 
 if __name__ == "__main__":
@@ -58,14 +129,15 @@ if __name__ == "__main__":
     client = CassandraClient(host, port, keyspace)
     client.connect()
 
-    header = ['domain', 'page_id', 'page_title',
-              'review_date', 'user_id', 'user_is_bot', 'user_text']
-    data = read_from_cassandra()
-    # print(data)
-    df = spark.createDataFrame(data)
-    # print(df.show())
-
-    # call every hour
-    rerport1(df)
+    # # call every hour
+    scheduler = BlockingScheduler()
+    df = read_from_cassandra()
+    scheduler.add_job(rerport1, 'interval', hours=1)
+    scheduler.add_job(rerport2, 'interval', hours=1)
+    scheduler.add_job(rerport3, 'interval', hours=1)
+    scheduler.start()
+    # rerport1()
+    # rerport2()
+    # rerport3()
 
     client.close()
